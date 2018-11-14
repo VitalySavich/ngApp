3d58ab2d73dc16df9f671c25bfa5f56f59161a51
VitalySavich <sviprogrammer@rambler.ru>
2018-11-14 15:41:28 +0300

    Video CRUD api

--- server/routes/api.js
+++ server/routes/api.js
@@ -40,5 +40,50 @@ router.get('/videos/:id', function(req, res){
     });
 });
 
+router.post('/video', function(req, res){
+    console.log('Post a video');
+    var newVideo = new Video();
+    newVideo.title = req.body.title;
+    newVideo.url = req.body.url;
+    newVideo.description = req.body.description;
+    newVideo.save(function(err, insertedVideo){
+        if (err){
+            console.log('Error saving video');
+        }else{
+            res.json(insertedVideo);
+        }
+    });
+});
+
+router.put('/video/:id', function(req, res){
+    console.log('Update a video');
+    Video.findByIdAndUpdate(req.params.id,
+    {
+        $set: {title: req.body.title, url: req.body.url, description: req.body.description}
+    },
+    {
+        new: true
+    },
+    function(err, updatedVideo){
+        if(err){
+            res.send("Error updating video");
+        }else{
+            res.json(updatedVideo);
+        }
+    }
+
+    );
+});
+
+router.delete('/video/:id', function(req, res){
+    console.log('Deleting a video');
+    Video.findByIdAndRemove(req.params.id, function(err, deletedVideo){
+        if(err){
+            res.send("Error deleting video");
+        }else{
+            res.json(deletedVideo);
+        }
+    });
+});
 
 module.exports = router;
