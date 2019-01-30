0c6d393d4e4a7f603d4e6e86e33e72122702212e
VitalySavich <sviprogrammer@rambler.ru>
2019-01-30 11:10:15 +0300

    User register and login API

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 const Video = require('../models/video');
 
 const db = "mongodb://vitali:87vitali87@ds163013.mlab.com:63013/videoplayer";
@@ -15,6 +17,40 @@ mongoose.connect(db, function(err){
 router.get('/', function(req, res){
     res.send('api works');
 });
+
+router.post('/register', (req, res) => {
+    let userData = req.body
+    let user = new User(userData)
+    user.save((err, registeredUser) => {
+      if (err) {
+        console.log(err)      
+      } else {
+        let payload = {subject: registeredUser._id}
+        let token = jwt.sign(payload, 'secretKey')
+        res.status(200).send({token})
+      }
+    })
+  })
+  
+router.post('/login', (req, res) => {
+    let userData = req.body
+    User.findOne({email: userData.email}, (err, user) => {
+      if (err) {
+        console.log(err)    
+      } else {
+        if (!user) {
+          res.status(401).send('Invalid Email')
+        } else 
+        if ( user.password !== userData.password) {
+          res.status(401).send('Invalid Password')
+        } else {
+          let payload = {subject: user._id}
+          let token = jwt.sign(payload, 'secretKey')
+          res.status(200).send({token})
+        }
+      }
+    })
+  })
 
 router.get('/videos', function(req, res){
     console.log('Get request for all videos');
